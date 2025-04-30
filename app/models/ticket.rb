53d42aa590764f252dfbc77fb475dcdbcaaa9321
Ademar Neto <ademarnetodev@gmail.com>
2025-04-29 22:51:00 -0300

    feat: define status padrão como 'aberto' e permite edição no formulário de edição

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,15 @@
 class Ticket < ApplicationRecord
   belongs_to :user
 
+  before_validation :set_default_status, on: :create
+
   validates :titulo, presence: true
-  validates :descricao, presence: true
-  validates :status, presence: true
+  validates :descricao, presence: true  
   validates :user_id, presence: true
+
+  private
+
+  def set_default_status
+    self.status ||= "aberto"
+  end
 end
